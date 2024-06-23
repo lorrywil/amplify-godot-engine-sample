e3ac5e680a1faffe5b1ecce483e9c558e285ffc9
Frederic Nowak <fredenow@amazon.com>
2024-06-23 15:15:22 +0200

    add the game image on the landing page and remove React logo

--- website/src/App.js
+++ website/src/App.js
@@ -1,21 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Squash the Creeps
         </p>
+        <img src="squash-the-creeps-final.webp"></img>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <br></br>
+          Play
         </a>
       </header>
     </div>

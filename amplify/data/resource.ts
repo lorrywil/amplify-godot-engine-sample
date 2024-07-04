ebe9e1f1c66fe5b5606c6092040076821dd33909
Nassim Benmalek <nassim.benmalek@gmail.com>
2024-07-04 14:01:29 +0200

    chore: add authorization to resource schema #17

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -7,7 +7,7 @@ const schema = a.schema({
     score: a.integer().required(),
     rank: a.integer(),
     lastUpdated: a.datetime()
-  })
+  }).authorization(allow => [allow.authenticated()])
 });
 
 export type Schema = ClientSchema<typeof schema>;

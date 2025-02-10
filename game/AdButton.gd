11209bc7efecfe82a670517092b5ffe07242821d
Frederic Nowak <fredenow@amazon.com>
2025-02-10 11:34:57 +0100

    add death animation for player

--- game/AdButton.gd
+++ game/AdButton.gd
@@ -1,7 +1,5 @@
 class_name AdButton
 extends Button
 
-signal profile_selected
-
 @onready var label: Label = %Label
 @onready var image: TextureRect = %Image
--- game/MusicPlayer.gd
+++ game/MusicPlayer.gd
@@ -3,9 +3,10 @@ extends Node
 
 const PIRATES_VS_SHARKS_ADVENTURE_1 = preload("res://musics/Pirates vs Sharks Adventure 1.mp3")
 const PIRATES_VS_SHARKS_ADVENTURE_2 = preload("res://musics/Pirates vs Sharks Adventure 2.mp3")
-
 const PIRATES_VS_SHARKS_BEACH_1 = preload("res://musics/Pirates vs Sharks Beach 1.mp3")
 const PIRATES_VS_SHARKS_BEACH_2 = preload("res://musics/Pirates vs Sharks Beach 2.mp3")
+const PIRATES_VS_SHARKS_KINGS_OF_THE_WORLD_1 = preload("res://musics/Pirates vs Sharks Kings of the World 1.mp3")
+const PIRATES_VS_SHARKS_KINGS_OF_THE_WORLD_2 = preload("res://musics/Pirates vs Sharks Kings of the World 2.mp3")
 
 const PIRATES_VS_SHARKS_TITLE = preload("res://musics/Pirates vs Sharks Title.mp3")
 const PIRATES_VS_SHARKS_LOOP = preload("res://musics/Pirates vs Sharks Loop.mp3")

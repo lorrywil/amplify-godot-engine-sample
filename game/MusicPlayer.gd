9aa9795a338fe61af469c528ce54ce63774a3111
Frederic Nowak <fredenow@amazon.com>
2025-02-10 07:21:11 +0100

    Add an Amazon Nova Reel generated video in the title screen

--- game/MusicPlayer.gd
+++ game/MusicPlayer.gd
@@ -4,6 +4,9 @@ extends Node
 const PIRATES_VS_SHARKS_ADVENTURE_1 = preload("res://musics/Pirates vs Sharks Adventure 1.mp3")
 const PIRATES_VS_SHARKS_ADVENTURE_2 = preload("res://musics/Pirates vs Sharks Adventure 2.mp3")
 
+const PIRATES_VS_SHARKS_BEACH_1 = preload("res://musics/Pirates vs Sharks Beach 1.mp3")
+const PIRATES_VS_SHARKS_BEACH_2 = preload("res://musics/Pirates vs Sharks Beach 2.mp3")
+
 const PIRATES_VS_SHARKS_TITLE = preload("res://musics/Pirates vs Sharks Title.mp3")
 const PIRATES_VS_SHARKS_LOOP = preload("res://musics/Pirates vs Sharks Loop.mp3")
 const PIRATES_VS_SHARKS_COMMERCIAL = preload("res://musics/Pirates vs Sharks Commercial.mp3")
--- game/Title.gd
+++ game/Title.gd
@@ -2,11 +2,22 @@ extends TextureRect
 
 const PROFILING = "res://Profiling.tscn"
 
+@onready var timer: Timer = $Timer
+@onready var video: VideoStreamPlayer = $Video
+@onready var speech: AudioStreamPlayer = $Speech
+@onready var welcome: ColorRect = $Welcome
 @onready var start: Button = $Welcome/Start
+@onready var player: AnimationPlayer = $Player
 
 func _ready() -> void:
 	start.grab_focus()
 	music_player.play_title()
 
 func _on_start_pressed() -> void:
+	timer.start(5)
+	video.play()
+	speech.play()
+	player.play("hide")
+
+func _on_timer_timeout():
 	get_parent().change_scene(PROFILING, true)

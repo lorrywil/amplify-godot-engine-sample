c2d626cfc9abf8c3e238c3c8c119f8e5506789d2
Lawrie <lawriewi@amazon.co.uk>
2025-03-04 10:46:15 +0000

    pie chart fixes

--- game/Game.gd
+++ game/Game.gd
@@ -241,9 +241,8 @@ func _on_commercial_c_pressed() -> void:
 	_on_commercial_pressed() 
 
 func _on_commercial_pressed() -> void:
-	
-	commercial_container.visible = false
 	var clicks = await GameAnalytics.query()
+	commercial_container.visible = false
 	commercial_statistics_container.visible = true
 	commercial_statistics_pie_chart.values = clicks
 	commercial_statistics_pie_chart.start_animation()
